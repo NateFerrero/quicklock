332c27a0562c5a95af49da577d9431c2b4cfcd14
Nate Ferrero <nateferrero@gmail.com>
2015-01-27 22:38:49 -0800

    Version bump to 0.1.7 and fix dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from distutils.core import setup
 setup(
   name = 'quicklock',
   packages = ['quicklock'],
-  version = '0.1.6',
+  version = '0.1.7',
   description = 'A simple Python resource lock to ensure only one process at a time is operating with a particular resource.',
   author = 'Nate Ferrero',
   author_email = 'nateferrero@gmail.com',
   url = 'https://github.com/NateFerrero/quicklock',
-  download_url = 'https://github.com/NateFerrero/quicklock/tarball/0.1.6',
+  download_url = 'https://github.com/NateFerrero/quicklock/tarball/0.1.7',
   keywords = ['lock', 'locking', 'singleton', 'process', 'resource', 'exclusive lock'],
   classifiers = [],
+  platforms='any',
+  install_requires = [
+    'psutil>=2.2'
+  ]
 )
