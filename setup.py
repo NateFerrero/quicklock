5cdc921b95657c1b92585169482130bbd5fed6e2
Nate Ferrero <nateferrero@gmail.com>
2015-01-27 22:22:12 -0800

    Remove .rst extension from README so PyPI will like it

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'quicklock',
   packages = ['quicklock'],
-  version = '0.1.4',
+  version = '0.1.5',
   description = 'A simple Python resource lock to ensure only one process at a time is operating with a particular resource.',
   author = 'Nate Ferrero',
   author_email = 'nateferrero@gmail.com',
   url = 'https://github.com/NateFerrero/quicklock',
-  download_url = 'https://github.com/NateFerrero/quicklock/tarball/0.1.4',
+  download_url = 'https://github.com/NateFerrero/quicklock/tarball/0.1.5',
   keywords = ['lock', 'locking', 'singleton', 'process', 'resource', 'exclusive lock'],
   classifiers = [],
 )
