8c4eb72cafc9d27cc70b1bd44046c635660764f7
Nate Ferrero <nateferrero@gmail.com>
2015-01-27 22:09:50 -0800

    Updates for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'quicklock',
+  packages = ['quicklock'],
+  version = '0.1.0',
+  description = 'A simple Python resource lock to ensure only one process at a time is operating with a particular resource.',
+  author = 'Nate Ferrero',
+  author_email = 'nateferrero@gmail.com',
+  url = 'https://github.com/NateFerrero/quicklock', # use the URL to the github repo
+  download_url = 'https://github.com/NateFerrero/quicklock/tarball/0.1.0',
+  keywords = ['lock', 'locking', 'singleton', 'process', 'resource', 'exclusive lock'],
+  classifiers = [],
+)
