da104b327f96de8e99c274eeb217beae5e71c455
Nate Ferrero <nateferrero@gmail.com>
2015-01-27 22:10:34 -0800

    Version bump to 0.1.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'quicklock',
   packages = ['quicklock'],
-  version = '0.1.0',
+  version = '0.1.2',
   description = 'A simple Python resource lock to ensure only one process at a time is operating with a particular resource.',
   author = 'Nate Ferrero',
   author_email = 'nateferrero@gmail.com',
   url = 'https://github.com/NateFerrero/quicklock', # use the URL to the github repo
-  download_url = 'https://github.com/NateFerrero/quicklock/tarball/0.1.0',
+  download_url = 'https://github.com/NateFerrero/quicklock/tarball/0.1.2',
   keywords = ['lock', 'locking', 'singleton', 'process', 'resource', 'exclusive lock'],
   classifiers = [],
 )
